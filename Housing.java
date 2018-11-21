bd510cdc9c3137cb0abb0e0cca62a0df8db0f03d
Adoomy96 <44295930+Adoomy96@users.noreply.github.com>
2018-11-21 12:46:29 +0000

    Add files via upload

---
+++ Housing.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package housingapp;
+
+/**
+ *
+ * @author x18100252
+ */
+public class Housing {
+    
+}
